573caea9d15ab12f75b95e2571c9757ba07d973d
baha-udin <you@example.com>
2020-12-23 15:18:57 +0700

    slicing category, body, & more

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,3 +5,11 @@ function on() {
     keranjangBelanja.classList.toggle("StyleKeranjangBelanja")
     overlay.classList.toggle("styleOverlay")
 }
+
+function buttonKategori() {
+    let kategori = document.getElementById("ambilKategori")
+    let overlay = document.getElementById("overlay")
+
+    kategori.classList.toggle("klik-category")
+    overlay.classList.toggle("styleOverlay")
+}
